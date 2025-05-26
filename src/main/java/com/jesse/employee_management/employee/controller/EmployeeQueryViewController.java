03113c65519437e3372bb9389d012e535cb20bfb
浩进 朱 <3191955858@qq.com>
2025-05-26 09:48:38 +0800

    员工查询视图控制器类。

---
+++ src/main/java/com/jesse/employee_management/employee/controller/EmployeeQueryViewController.java
@@ -0,0 +1,61 @@
+package com.jesse.employee_management.employee.controller;
+
+import com.jesse.employee_management.employee.service.EmployeeServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping(path = "/employee")
+public class EmployeeQueryViewController
+{
+    private final EmployeeServiceInterface employeeService;
+
+    @Autowired
+    public EmployeeQueryViewController(EmployeeServiceInterface emp) {
+        this.employeeService = emp;
+    }
+
+    @GetMapping(path = "/query")
+    String employeeQueryPage() {
+        return "Employee_Query";
+    }
+
+    @GetMapping(path = "/query/{employeeId}")
+    String specifiedEmployeeQueryPage(
+            Model model,
+            @PathVariable Integer employeeId
+    )
+    {
+        try
+        {
+            model.addAttribute(
+                    "EmployeeInfo",
+                    this.employeeService.getEmployeeWholeInfoById(employeeId)
+            );
+        }
+        catch (RuntimeException exception)
+        {
+            log.error(exception.getMessage());
+        }
+
+        return "Specified_Employee_Query";
+    }
+
+    @GetMapping(path = "/pagination_query")
+    String employeeQueryByRangePage(Model model)
+    {
+        // 获取员工总数
+        model.addAttribute(
+                "EmployeeAmount",
+                this.employeeService.getEmployeeAmount()
+        );
+
+        return "Employee_Pagination_Query";
+    }
+}
